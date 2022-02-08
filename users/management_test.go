6754bf1b0f281d8756719670581bdb7f14d6545c
Rhine Cliff <rhinecliff@protonmail.com>
2022-02-07 22:18:34 -0800

    Add unit-testing for the user / role management permission testing

--- users/management_test.go
+++ users/management_test.go
@@ -291,6 +291,198 @@ func TestManagingUsers(t *testing.T) {
 	}
 }
 
+func TestUserPermissionChecking(assignedPermissions *testing.T) {
+	assert := assert.New(assignedPermissions)
+	log.SetLevel(log.DebugLevel)
+
+	dbName := fmt.Sprintf("/tmp/models_test_%s.db", uuid.New().String())
+	log.Debugf("Unit-test DB %s", dbName)
+	db, err := gorm.Open(sqlite.Open(dbName), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	assert.Nil(err)
+	supportMatch, err := common.GetCustomFieldValidator(
+		`^[a-zA-Z0-9-]+$`, `^[a-zA-Z0-9-]+$`, `^[a-zA-Z0-9-]+$`, `^[a-zA-Z0-9-]+$`, `^.+$`,
+	)
+	assert.Nil(err)
+	dbClient, err := models.CreateManagementDBClient(db, supportMatch)
+	assert.Nil(err)
+	assert.Nil(dbClient.Ready())
+
+	uut, err := CreateManagement(dbClient)
+	assert.Nil(err)
+	assert.Nil(uut.Ready())
+
+	roles := make([]string, 5)
+	for itr := 0; itr < 5; itr++ {
+		roles[itr] = uuid.New().String()
+	}
+
+	permissions := make([]string, 8)
+	for itr := 0; itr < 8; itr++ {
+		permissions[itr] = uuid.New().String()
+	}
+
+	testRoles := map[string]common.UserRoleConfig{
+		roles[0]: {AssignedPermissions: []string{permissions[0], permissions[1], permissions[2]}},
+		roles[1]: {AssignedPermissions: []string{permissions[2], permissions[3]}},
+		roles[2]: {AssignedPermissions: []string{permissions[1], permissions[4], permissions[5]}},
+		roles[3]: {AssignedPermissions: []string{permissions[0], permissions[6]}},
+		roles[4]: {AssignedPermissions: []string{permissions[7]}},
+	}
+	assert.Nil(uut.AlignRolesWithConfig(context.Background(), testRoles))
+
+	userID := uuid.New().String()
+	{
+		param := models.UserConfig{UserID: userID}
+		assert.Nil(uut.DefineUser(context.Background(), param, []string{}))
+	}
+
+	getPermissionsOfRoles := func(
+		associatedRoles map[string]common.UserRoleConfig, missingRoles map[string]bool,
+	) ([]string, []string) {
+		associatedPermissions := map[string]bool{}
+		for aRole := range associatedRoles {
+			for _, permission := range testRoles[aRole].AssignedPermissions {
+				associatedPermissions[permission] = true
+			}
+		}
+		notAssociatedPermissions := map[string]bool{}
+		for aRole := range missingRoles {
+			for _, permission := range testRoles[aRole].AssignedPermissions {
+				if _, ok := associatedPermissions[permission]; !ok {
+					notAssociatedPermissions[permission] = true
+				}
+			}
+		}
+		assignedPermissions := make([]string, 0, len(associatedPermissions))
+		for permission := range associatedPermissions {
+			assignedPermissions = append(assignedPermissions, permission)
+		}
+		missingPermissions := make([]string, 0, len(notAssociatedPermissions))
+		for permission := range notAssociatedPermissions {
+			missingPermissions = append(missingPermissions, permission)
+		}
+		return assignedPermissions, missingPermissions
+	}
+
+	type testCase struct {
+		roles map[string]common.UserRoleConfig
+	}
+
+	// Case 0: test changes to user assigned roles
+	{
+		tests := []testCase{
+			{roles: map[string]common.UserRoleConfig{
+				roles[0]: testRoles[roles[0]],
+			}},
+			{roles: map[string]common.UserRoleConfig{
+				roles[1]: testRoles[roles[1]],
+			}},
+			{roles: map[string]common.UserRoleConfig{
+				roles[0]: testRoles[roles[0]], roles[3]: testRoles[roles[3]],
+			}},
+			{roles: map[string]common.UserRoleConfig{
+				roles[4]: testRoles[roles[4]], roles[2]: testRoles[roles[2]],
+			}},
+			{roles: map[string]common.UserRoleConfig{
+				roles[3]: testRoles[roles[3]],
+			}},
+			{roles: map[string]common.UserRoleConfig{
+				roles[2]: testRoles[roles[2]], roles[4]: testRoles[roles[4]],
+			}},
+		}
+
+		for _, oneTest := range tests {
+			missingRoles := map[string]bool{}
+			for _, aRole := range roles {
+				if _, ok := oneTest.roles[aRole]; !ok {
+					missingRoles[aRole] = true
+				}
+			}
+			newRoles := make([]string, 0, len(oneTest.roles))
+			for aRole := range oneTest.roles {
+				newRoles = append(newRoles, aRole)
+			}
+			assigned, missing := getPermissionsOfRoles(oneTest.roles, missingRoles)
+			assert.Nil(uut.SetUserRoles(context.Background(), userID, newRoles))
+			// Test permission which should be there
+			for _, permission := range assigned {
+				havePermission, err := uut.DoesUserHavePermission(
+					context.Background(), userID, []string{permission},
+				)
+				assert.Nil(err)
+				assert.True(havePermission)
+			}
+			// Test permission which should be missing
+			for _, permission := range missing {
+				havePermission, err := uut.DoesUserHavePermission(
+					context.Background(), userID, []string{permission},
+				)
+				assert.Nil(err)
+				assert.False(havePermission)
+			}
+		}
+	}
+
+	// Case 1: test changes roles available in instance
+	{
+		tests := []testCase{
+			{roles: map[string]common.UserRoleConfig{
+				roles[0]: testRoles[roles[0]],
+			}},
+			{roles: map[string]common.UserRoleConfig{
+				roles[1]: testRoles[roles[1]],
+			}},
+			{roles: map[string]common.UserRoleConfig{
+				roles[0]: testRoles[roles[0]], roles[3]: testRoles[roles[3]],
+			}},
+			{roles: map[string]common.UserRoleConfig{
+				roles[4]: testRoles[roles[4]], roles[2]: testRoles[roles[2]],
+			}},
+			{roles: map[string]common.UserRoleConfig{
+				roles[3]: testRoles[roles[3]],
+			}},
+			{roles: map[string]common.UserRoleConfig{
+				roles[2]: testRoles[roles[2]], roles[4]: testRoles[roles[4]],
+			}},
+		}
+
+		for idx, oneTest := range tests {
+			// Start at initial set of roles
+			assert.Nil(uut.AlignRolesWithConfig(context.Background(), testRoles))
+			// Give user all roles
+			assert.Nil(uut.SetUserRoles(context.Background(), userID, roles))
+			// Change the roles available in the system
+			assert.Nil(uut.AlignRolesWithConfig(context.Background(), oneTest.roles))
+
+			missingRoles := map[string]bool{}
+			for _, aRole := range roles {
+				if _, ok := oneTest.roles[aRole]; !ok {
+					missingRoles[aRole] = true
+				}
+			}
+			assigned, missing := getPermissionsOfRoles(oneTest.roles, missingRoles)
+			// Test permission which should be there
+			for _, permission := range assigned {
+				havePermission, err := uut.DoesUserHavePermission(
+					context.Background(), userID, []string{permission},
+				)
+				assert.Nil(err)
+				assert.Truef(havePermission, "%d", idx)
+			}
+			// Test permission which should be missing
+			for _, permission := range missing {
+				havePermission, err := uut.DoesUserHavePermission(
+					context.Background(), userID, []string{permission},
+				)
+				assert.Nil(err)
+				assert.Falsef(havePermission, "%d", idx)
+			}
+		}
+	}
+}
+
 func roleListToMap(i []string) map[string]bool {
 	result := map[string]bool{}
 	for _, e := range i {
